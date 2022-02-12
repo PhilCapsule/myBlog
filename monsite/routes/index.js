c822d96c4e3c15664e2f2c338bd0a4e2fce64888
Phil <phildev013@gmail.com>
2022-02-12 20:13:14 +0100

    route+responsive_index

--- monsite/routes/index.js
+++ monsite/routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/erreur404', function(req, res, next){
+  res.render('erreur');
+});
+
+router.get('/boutique', function(req, res, next){
+  res.render('boutique')
+});
+
+router.get('/voyager', function(req,res,next){
+  res.render('voyager')
+});
+
+router.get('/meteo', function(req,res,next){
+  res.render('meteo')
+})
+
+// voir if redirect
+router.post('/log-in', function(req,res,next){
+  res.render('log-in')
+});
+
+
 module.exports = router;
+
+
