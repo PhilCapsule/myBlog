6f7479a5faf37cfa070df48e38e8ae10a5136e4a
Phil <phildev013@gmail.com>
2022-02-18 15:27:07 +0100

    add path

--- monsite/routes/index.js
+++ monsite/routes/index.js
@@ -2,15 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 // /fakeDB 
+//add humidité 
+var cityList = [
+    {name: "marseille", desc:"couvert", img:"images/picto-1.png", temp_min: 2, temp_max: 8 },
+    {name: "Aberdeen", desc:"couvert", img:"images/picto-1.png", temp_min: -3, temp_max: 5 },
+    {name: "Londres", desc:"couvert", img:"images/picto-1.png", temp_min: 4, temp_max: 7 },
+    {name: "Hampshire", desc:"couvert", img:"images/picto-1.png", temp_min: 1, temp_max: 10 },
+]
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/erreur404', function(req, res, next){
-  res.render('erreur');
-});
+
+// add btn for path 
+// router.get('/erreur404', function(req, res, next){
+//   res.render('erreur');
+// });
 
 router.get('/boutique', function(req, res, next){
   res.render('boutique')
@@ -21,7 +32,7 @@ router.get('/voyager', function(req,res,next){
 });
 
 router.get('/meteo', function(req,res,next){
-  res.render('meteo')
+  res.render('meteo', {cityList})
 })
 
 // voir if redirect  et POST 
