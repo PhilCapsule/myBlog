24cceb7637e2b435b57f9563e263c9a3372420d4
Phil <phildev013@gmail.com>
2022-02-26 14:31:08 +0100

    display API map city

--- monsite/public/javascripts/script.js
+++ monsite/public/javascripts/script.js
@@ -1,9 +1,6 @@
-var mymap = L.map('mapid',{
-    center: [48.866667, 2,333333],
-    zoom: 4,
-});
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
 
 // var cities = document.getElementsByClassName('list-group-item');
